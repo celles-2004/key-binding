e279143be3469d511c8f52a97d9a92b54a41b07d
celles-2004 <asr511246das1@gmail.com>
2026-08-04 13:14:11 +0800

    рабочий ребинд

--- simple_remap.py
+++ simple_remap.py
@@ -1,18 +1,21 @@
 from pynput import keyboard
-from pynput.keyboard import Controller
+from pynput.keyboard import Controller, KeyCode
 
 ctrl = Controller()
+
 
 def on_press(key):
     try:
         if key.char == 'a':
             print('a нажата -> отправляем b')
-            ctrl.press('b')
-            ctrl.release('b')
+            target = KeyCode.from_char('b')
+            ctrl.press(target)
+            ctrl.release(target)
             return False  # подавляем a
     except AttributeError:
         pass
     return True
+
 
 print("Переназначение a -> b глобально. Нажми Ctrl+C для выхода.")
 with keyboard.Listener(on_press=on_press) as listener:
